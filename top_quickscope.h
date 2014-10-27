f92364a15045b3088540530de06268e348334652
Lance Arsenault <lanceman@carpenter.greendale>
2014-10-27 15:37:54 -0400

    Fixed make dist

--- top_quickscope.h
+++ top_quickscope.h
@@ -2,6 +2,12 @@
  * Copyright (C) 2012-2014  Lance Arsenault
  * GNU General Public License version 3
  */
+/* This file is the top file to be concatenated
+ * with other files to make libquickscope installed
+ * quickscope.h header file.
+ *
+ * TODO: quickscope.h could be much shorter.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
